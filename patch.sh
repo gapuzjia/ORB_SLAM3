217a0e4dcf80276715dc5ea8d07bc3d1c39a4c5a
root <jiannagapuz@gmail.com>
2025-07-11 13:15:43 -0700

    run horizontal and checkerboard mask, successful results. delete previous runs

--- patch.sh
+++
@@ -1,8 +0,0 @@
-for file in ./Examples/Stereo-Inertial/*.yaml; do
-  if ! grep -q "Viewer.Active" "$file"; then
-    echo "Viewer.Active: 0" >> "$file"
-  else
-    sed -i 's/^Viewer\.Active:.*/Viewer.Active: 0/' "$file"
-  fi
-done
-
