3b7d2307a87d4f48acd80691b660bf190fb9101c
root <root@jetson.attlocal.net>
2025-07-19 17:46:11 +0000

    modify configs to only do tests on normal datasets

--- run_mask_random.sh
+++ run_mask_random.sh
@@ -4,15 +4,16 @@ set -e  # Exit on error
 
 MASK_NAME="MaskRandom"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-NUM_RUNS=1
+NUM_RUNS=10
 
 DATASETS=("MH01" "MH02" "MH03" "MH04" "MH05" "V101" "V102" "V103" "V201" "V202" "V203")
 
+#remove extra configs, do tests on only normal
 CONFIGURATIONS=(
-  "./Examples/Stereo-Inertial/EuRoC_oasis.yaml result_oasis"
-  "./Examples/Stereo-Inertial/EuRoC_deadlines.yaml result_deadlines"
-  "./Examples/Stereo-Inertial/EuRoC_fov_deadlines.yaml result_fov_deadlines"
-  "./Examples/Stereo-Inertial/EuRoC_fov.yaml result_fov"
+  #"./Examples/Stereo-Inertial/EuRoC_oasis.yaml result_oasis"
+  #"./Examples/Stereo-Inertial/EuRoC_deadlines.yaml result_deadlines"
+  #"./Examples/Stereo-Inertial/EuRoC_fov_deadlines.yaml result_fov_deadlines"
+  #"./Examples/Stereo-Inertial/EuRoC_fov.yaml result_fov"
   "./Examples/Stereo-Inertial/EuRoC.yaml result_normal"
 )
 
